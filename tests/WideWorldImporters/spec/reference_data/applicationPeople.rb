06b89f0052f7236c2485da7c8073be247e230c5d
Eric Kang <erickangatms@outlook.com>
2017-08-05 23:00:36 -0700

    .

---
+++ tests/WideWorldImporters/spec/reference_data/applicationPeople.rb
@@ -0,0 +1,5 @@
+describe 'WideWorldImporters: reference table: Application.People' do
+    it 'contains PersonID == 1 template' do
+        expect(sql.WideWorldImporters.reference_data.check_personid(:id => 1)).to match('WideWorldImporters/reference_data/match/application_people_personid_1.csv')
+    end
+end
