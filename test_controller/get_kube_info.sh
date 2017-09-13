42e51344e066d91fb74a1151ccbaa7c79d0db90d
Eric Kang <erickangatms@outlook.com>
2017-09-13 15:56:27 -0700

    .

--- test_controller/get_kube_info.sh
+++ test_controller/get_kube_info.sh
@@ -5,5 +5,5 @@
 # deployed sqldevops namespace
 # and slacker service is deployed as NodePort type
 
-kubectl get services -n sqldevops
+kubectl get services -n $kube_namespace 
 kubectl describe nodes | grep -e 'Name:' -e 'IP' -e 'Addresses' -e 'slacker' -e 'webapp' -e 'sqltools' -e 'mssql'
