7cb68251ac6c597650cfc0d7ee40929c800ee859
Eric Kang <erickangatms@outlook.com>
2017-09-14 12:17:52 -0700

    unified server name to mssql

--- docker_cluster/mssql_jobs/provisioning/provision.sh
+++ docker_cluster/mssql_jobs/provisioning/provision.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sqlcmd -Ssql -Usa -PYukon900 -i ./restore.sql
-sqlcmd -Ssql -Usa -PYukon900 -i ./adventureworks-restore.sql
+sqlcmd -Smssql -Usa -PYukon900 -i ./restore.sql
+sqlcmd -Smssql -Usa -PYukon900 -i ./adventureworks-restore.sql
