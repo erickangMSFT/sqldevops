b3f3fdb905df93d4e980c2c7ddd9a4ffdb884e7d
Eric Kang <erickangatms@outlook.com>
2017-09-14 08:07:10 -0700

    .

--- docker_cluster/pgsql_jobs/provisioning/provisioning.sh
+++ docker_cluster/pgsql_jobs/provisioning/provisioning.sh
@@ -1,4 +1,9 @@
-#!/bin/bash
-createdb -U sa pagila
-psql -U sa -f /backup/pagila-0.10.1/pagila-schema.sql pagila 
-psql -U sa -f /backup/pagila-0.10.1/pagila-insert-data.sql pagila
+#!/bin/sh
+
+RUN wget -q https://ftp.postgresql.org/pub/projects/pgFoundry/dbsamples/pagila/pagila/pagila-0.10.1.zip
+RUN unzip ./pagila-0.10.1.zip
+RUN rm ./pagila-0.10.1.zip
+
+createdb -h pgsql -U sa pagila
+psql -h pgsql -U sa -f ./pagila-0.10.1/pagila-schema.sql pagila 
+psql -h pgsql -U sa -f ./pagila-0.10.1/pagila-insert-data.sql pagila
