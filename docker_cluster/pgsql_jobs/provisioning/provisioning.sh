d18d49a06dd842996c21555c2d961566e2b601c6
Eric Kang <erickang@linux-vm.localdomain>
2017-09-14 10:27:28 -0700

    .

--- docker_cluster/pgsql_jobs/provisioning/provisioning.sh
+++ docker_cluster/pgsql_jobs/provisioning/provisioning.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
-RUN wget -q https://ftp.postgresql.org/pub/projects/pgFoundry/dbsamples/pagila/pagila/pagila-0.10.1.zip
-RUN unzip ./pagila-0.10.1.zip
-RUN rm ./pagila-0.10.1.zip
+print ** downloading pagila sample database
+wget -q https://ftp.postgresql.org/pub/projects/pgFoundry/dbsamples/pagila/pagila/pagila-0.10.1.zip
+unzip ./pagila-0.10.1.zip
+rm ./pagila-0.10.1.zip
 
+print ** creating pagila database
 psql -h pgsql -U sa -c "CREATE DATABASE pagila;"
+
+print ** creating schema in pagila database
 psql -h pgsql -U sa -f ./pagila-0.10.1/pagila-schema.sql pagila 
+
+print ** inserting data to pagila database
 psql -h pgsql -U sa -f ./pagila-0.10.1/pagila-insert-data.sql pagila
