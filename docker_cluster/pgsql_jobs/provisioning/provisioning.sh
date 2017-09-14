b470976680a2a7caed911e334fdc7fe1121f7330
Eric Kang <erickang@linux-vm.localdomain>
2017-09-14 11:05:15 -0700

    pg sql provisioning job

--- docker_cluster/pgsql_jobs/provisioning/provisioning.sh
+++ docker_cluster/pgsql_jobs/provisioning/provisioning.sh
@@ -1,9 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 print ** downloading pagila sample database
 wget -q https://ftp.postgresql.org/pub/projects/pgFoundry/dbsamples/pagila/pagila/pagila-0.10.1.zip
 unzip ./pagila-0.10.1.zip
 rm ./pagila-0.10.1.zip
+
+PGPASSWORD='Yukon900'
 
 print ** creating pagila database
 psql -h pgsql -U sa -c "CREATE DATABASE pagila;"
