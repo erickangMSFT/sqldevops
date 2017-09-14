8738aa6170d1afd98e49b537baa23bd88c14dbaa
Eric Kang <erickangatms@outlook.com>
2017-09-13 17:36:52 -0700

    demodb pg

--- docker_cluster/demodb/pgsql/docker_build_push.sh
+++ docker_cluster/demodb/pgsql/docker_build_push.sh
@@ -5,10 +5,7 @@ dt=`date '+%Y-%m-%d_%H-%M-%S'`
 # initial build including wwi backup files and restore script
 docker build . --rm -t demodb:pgsql
 
-
-docker login
-
 docker push ericskang/demodb:pgsql
 
 docker rmi -f $(docker images -f "dangling=true" -q)
-docker rmi -f demodb:pgsql
+
