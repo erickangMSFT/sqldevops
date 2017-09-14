64bfdc648023a84322d52868a33a64b11de98664
Eric Kang <erickang@linux-vm.localdomain>
2017-09-13 17:32:57 -0700

    demo db image

--- docker_cluster/demodb/mssql/docker_build_push.sh
+++ docker_cluster/demodb/mssql/docker_build_push.sh
@@ -3,12 +3,8 @@
 dt=`date '+%Y-%m-%d_%H-%M-%S'`
 
 # initial build including wwi backup files and restore script
-docker build . --rm -t demodb:mssql
-
-
-docker login
+docker build . --rm -t ericskang/demodb:mssql
 
 docker push ericskang/demodb:mssql
 
 docker rmi -f $(docker images -f "dangling=true" -q)
-docker rmi -f demodb:mssql
