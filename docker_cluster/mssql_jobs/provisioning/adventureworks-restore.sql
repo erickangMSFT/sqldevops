7a421ef48b1397434a508d35f9026dc692597f45
Eric Kang <erickangatms@outlook.com>
2017-09-14 11:32:45 -0700

    added temp backup folder outside of pv

--- docker_cluster/mssql_jobs/provisioning/adventureworks-restore.sql
+++ docker_cluster/mssql_jobs/provisioning/adventureworks-restore.sql
@@ -1,5 +1,5 @@
 RESTORE DATABASE [AdventureWorks] 
-FROM  DISK = N'/var/opt/mssql/backup/AdventureWorks2014.bak' 
+FROM  DISK = N'/tmp/backup/AdventureWorks2014.bak' 
 WITH  FILE = 1,  MOVE N'AdventureWorks2014_Data' 
 TO N'/var/opt/mssql/data/AdventureWorks_Data.mdf',  
 MOVE N'AdventureWorks2014_Log' 
--- docker_cluster/mssql_jobs/provisioning/restore.sql
+++ docker_cluster/mssql_jobs/provisioning/restore.sql
@@ -1,4 +1,4 @@
-RESTORE DATABASE WideWorldImporters FROM DISK = "/var/opt/mssql/backup/WideWorldImporters-Full.bak" 
+RESTORE DATABASE WideWorldImporters FROM DISK = "/tmp/backup/WideWorldImporters-Full.bak" 
 WITH MOVE "WWI_Primary" TO "/var/opt/mssql/data/WideWorldImporters.mdf", 
 MOVE "WWI_Userdata" TO "/var/opt/mssql/data/WideWorldImporters_Userdata.mdf", 
 MOVE "WWI_Log" TO "/var/opt/mssql/data/WideWorldImporters.ldf", MOVE "WWI_InMemory_Data_1" 
