81928cd486feeba8df9a4182d5e2795c0a5eb53a
Eric Kang <erickangMSFT@outlook.com>
2017-10-19 20:58:01 -0700

    .

---
+++ docker_cluster/kubernetes/nfs-provisioner/install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+kubectl apply -f deployment.yaml
+kubectl apply -f class.yaml
+kubectl apply -f auth/serviceaccount.yaml
+kubectl apply -f auth/clusterrole.yaml
+kubectl apply -f auth/clusterrolebinding.yaml
+
+kubectl patch deployment nfs-client-provisioner -p '{"spec":{"template":{"spec":{"serviceAccount":"nfs-client-provisioner"}}}}'
+
+
+kubectl apply -f pvc.yaml
+kubectl apply -f test.yaml
