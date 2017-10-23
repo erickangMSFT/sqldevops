860b133fc1d0b6ec0470335bca9e2c7eace7a5c3
Eric Kang <erickangatms@outlook.com>
2017-10-23 14:53:52 -0700

    .

--- install-powershell.sh
+++ install-powershell.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 
 # Register the Microsoft Ubuntu repository
-curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/microsoft.list
+curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | tee /etc/apt/sources.list.d/microsoft.list
 
 # Update the list of products
 apt-get update
