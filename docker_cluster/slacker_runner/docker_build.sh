bf5c5d3ee29f079fa95096b52efa4f84fe09da03
Eric Kang <erickangatms@outlook.com>
2017-10-23 14:16:05 -0700

    .

--- docker_cluster/slacker_runner/docker_build.sh
+++ docker_cluster/slacker_runner/docker_build.sh
@@ -2,4 +2,9 @@
 #wget https://raw.githubusercontent.com/exAspArk/docker-alpine-ruby/master/gemrc
 docker build . --rm -t slacker-node-runner:alpine
 
-docker rmi -f $(docker images -f "dangling=true" -q)
+dt=`date '+%Y-%m-%d_%H-%M-%S'`
+docker tag slacker-node-runner:alpine ericskang/slacker-node-runner:$dt
+docker tag ericskang/slacker-node-runner:$dt ericskang/slacker-node-runner:latest
+
+docker push ericskang/slacker-node-runner:$dt
+docker push ericskang/slacker-node-runner:latest
