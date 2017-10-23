2b64d3ac5ad424b51220c5fe894134b21d41829c
Eric Kang <erickangatms@outlook.com>
2017-10-23 14:53:35 -0700

    aded powershell install script

---
+++ install-powershell.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+
+# Register the Microsoft Ubuntu repository
+curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/microsoft.list
+
+# Update the list of products
+apt-get update
+
+# Install PowerShell
+apt-get install -y powershell
