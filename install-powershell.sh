2d51b3628c3a2b4df5ba24bd568c2badf918e264
Eric Kang <erickangatms@outlook.com>
2017-10-23 15:11:13 -0700

    added msql-tools head-less install

--- install-powershell.sh
+++ install-powershell.sh
@@ -4,8 +4,15 @@ curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 # Register the Microsoft Ubuntu repository
 curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | tee /etc/apt/sources.list.d/microsoft.list
 
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+
+curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | tee /etc/apt/sources.list.d/msprod.list
+
 # Update the list of products
 apt-get update
 
 # Install PowerShell
-apt-get install -y powershell
+apt-get install -y powershell mssql-tools unixodbc-dev
+
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
+source ~/.bashrc
