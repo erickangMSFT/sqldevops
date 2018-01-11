abae86637b48f457c49a8c52495e5d05c27a8945
Eric Kang <erickangatms@outlook.com>
2018-01-11 07:17:58 -0800

    .

--- docker_cluster/aks/3-aks-provisioning.sh
+++ docker_cluster/aks/3-aks-provisioning.sh
@@ -5,20 +5,25 @@ az provider register -n Microsoft.ContainerService
 az provider register -n Microsoft.Compute
 az provider register -n Microsoft.Network
 
-az group create -n mvademoaksgroup -l westus2
+az group create -n mvademok8sgroup -l westus2
 
-az aks create -g mvademoaksgroup \
-    -n mvademok8s \
-    --node-count 3 \
+az aks create -g mvademok8sgroup \
+    -n mvademok8scluster \
+    --node-count 1 \
     --node-vm-size Standard_D2s_v3 \
     --generate-ssh-keys
 
+# az aks scale \ 
+#     -g mvademok8sgroup \
+#     -n mvademok8scluster \
+#     --node-count 5 \
 
 az aks install-cli
 
 az aks get-credentials \
-    -g mvademoaksgroup \
-    -n mvademok8s
+    -g mvademok8sgroup \
+    -n mvademok8scluster
+    
 kubectl config get-contexts
 
 clear
--- docker_cluster/aks/5-resotre database.sh
+++ docker_cluster/aks/5-resotre database.sh
@@ -1,7 +1,10 @@
-kubectl exec -ti mssql-0 -n production \
-    bash -c 'cd /var/opt/mssql \
-        && mkdir -p backup \
-        && cd backup \
-        && wget https://github.com/Microsoft/sql-server-samples/releases/download/wide-world-importers-v0.2/WideWorldImporters-Full.bak \
-        && wget https://raw.githubusercontent.com/erickangMSFT/sqldevops/master/docker_cluster/aks/restore.sql \
-        && /opt/mssql-tools/bin/sqlcmd -Usa -PSqlDevOps2017 -i restore.sql'
+kubectl exec -ti mssql-0 -n production bash 
+
+#run following commands in the kubernetes container terminal
+cd /var/opt/mssql
+mkdir -p backup
+cd backup
+wget https://github.com/Microsoft/sql-server-samples/releases/download/wide-world-importers-v0.2/WideWorldImporters-Full.bak
+wget https://raw.githubusercontent.com/erickangMSFT/sqldevops/master/docker_cluster/aks/restore.sql
+/opt/mssql-tools/bin/sqlcmd -Usa -PSqlDevOps2017 -i restore.sql
+
